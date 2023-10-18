b6dd4e16525667d87f4f471c2354bc3f605e0f7a
Roman Silivestrov <rsilivestr@gmail.com>
2023-10-18 10:48:59 +0300

    Add element and variant props, styles to Text

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,14 +1,46 @@
 import cn from 'classnames';
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 import styles from './Text.module.scss';
 
-type Props = React.BaseHTMLAttributes<HTMLParagraphElement>;
+type TextVariant = 'h1' | 'h2' | 'h3' | 'body1';
 
-const Text: React.FC<Props> = ({ children, className, ...props }) => (
-  <p className={cn(styles.root, className)} {...props}>
-    {children}
-  </p>
-);
+type Props = React.BaseHTMLAttributes<HTMLParagraphElement> & {
+  align?: CSSProperties['textAlign'];
+  element?: React.ElementType;
+  variant: TextVariant;
+};
+
+const variantToElementMapping: Record<TextVariant, React.ElementType> = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  body1: 'p',
+};
+
+const Text: React.FC<Props> = ({
+  align,
+  children,
+  className,
+  element,
+  variant = 'body1',
+  ...props
+}) => {
+  const Element = element ?? variantToElementMapping[variant];
+
+  return (
+    <Element
+      className={cn(
+        styles.text,
+        styles[`text--variant_${variant}`],
+        align && [styles[`text--align_${align}`]],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Element>
+  );
+};
 
 export default Text;
